5d184a59641e6fc167937b941ada6e283fde0a3e
Elemento24 <mittalvishesh021@gmail.com>
2020-10-07 19:00:46 +0530

    Complete the Carousel Section

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ $(document).ready(() => {
   });
 
   // Owl-Carousel for Blog
-  $('.owl-carousel').owlCarousel();
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    // autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+  });
 
 });
