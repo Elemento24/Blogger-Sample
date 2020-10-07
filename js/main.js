9e1410f3cf24f6ef567ebf9d89f85398a971f2d2
Elemento24 <mittalvishesh021@gmail.com>
2020-10-07 21:08:23 +0530

    Make the Carousel Responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+  0: {
+    items: 1
+  },
+  320: {
+    items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3
+  }
+}
+
 $(document).ready(() => {
 
   $nav = $('.nav');
@@ -11,11 +26,12 @@ $(document).ready(() => {
   // Owl-Carousel for Blog
   $('.owl-carousel').owlCarousel({
     loop: true,
-    // autoplay: true,
+    autoplay: true,
     autoplayTimeout: 3000,
     dots: false,
     nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive
   });
 
   // Click to Scroll Top
